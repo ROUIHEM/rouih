92ada3c46f9cb1ccf009dbdc040326f9b72c88a7
rouih <roui0006@algonquinlive.com>
2023-08-01 18:13:52 -0400

    commuting all files

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Lab6.Models.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+string dbConnStr = builder.Configuration.GetConnectionString("StudentRecord");
+builder.Services.AddDbContext<StudentRecordContext>(options => options.UseSqlServer(dbConnStr));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
